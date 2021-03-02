753057698199b79ac359cd10090e1d607a93cf74
Alexander <bombblob@gmail.com>
2021-03-01 16:30:07 -0800

    format, added query for all emote data

--- src/content.js
+++ src/content.js
@@ -1,42 +1,76 @@
 // 'use strict'
 
-const axios = require('axios');
-const $ = require('jquery')
-const Buffer = require('buffer').Buffer
+const axios = require("axios");
+const $ = require("jquery");
+const Buffer = require("buffer").Buffer;
 
 // listen for msg from background script
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "click browser action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === "click browser action") {
+    var firstHref = $("a[href^='http']").eq(0).attr("href");
 
-      console.log(firstHref);
-    }
+    console.log(firstHref);
   }
-);
+});
 
-console.log("Hello from BYTV console")
+console.log("Hello from BYTV console");
 
 // TODO: only do things for YT links
 // TODO: intercept live chat messages
 
 const getRequest = async () => {
-	try {
-		const response = await axios.get('https://api.betterttv.net/3/cached/frankerfacez/users/twitch/71092938');
-		console.log("DATA", response)
-		console.log(response.data[0].images['4x']);
+  try {
+    const response = await axios.get(
+      "https://api.betterttv.net/3/cached/frankerfacez/users/twitch/71092938"
+    );
+    console.log("DATA", response);
+    console.log(response.data[0].images["4x"]);
 
-		const resp = await axios.get(response.data[0].images['4x'])
-		const d = resp.data
+    const resp = await axios.get(response.data[0].images["4x"]);
+    const d = resp.data;
 
-		const b64 = Buffer.from(d).toString("base64")
+    $("#logo-icon").html(
+      '<img id="logo-icon" class="style-scope ytd-topbar-logo-renderer" src=' +
+        response.data[0].images["4x"] +
+        " />"
+    );
 
-		$('#logo-icon').html('<img id="logo-icon" class="style-scope ytd-topbar-logo-renderer" src=' + response.data[0].images['4x'] + " />")
-	} catch (err) {
-		console.error(err);
-	}
+    var r1 = await axios.get(
+      "https://api.betterttv.net/3/cached/emotes/global"
+    );
+    var r2 = await axios.get(
+      "https://api.betterttv.net/3/cached/users/twitch/71092938"
+    );
+
+    const bttvEmoteData = r1.data
+      .concat(r2.data["channelEmotes"])
+      .concat(r2.data["sharedEmotes"]); // always has id,code,imageType
+    console.log("bttv:", bttvEmoteData);
+
+    r1 = await axios.get(
+      "https://api.betterttv.net/3/cached/frankerfacez/emotes/global"
+    );
+    r2 = await axios.get(
+      "https://api.betterttv.net/3/cached/frankerfacez/users/twitch/71092938"
+    );
+
+    const ffzEmoteData = r1.data.concat(r2.data); // always has id,code,imageType, and `images` field with links to source img
+    console.log("ffz:", ffzEmoteData);
+    return {
+      bttv: bttvEmoteData,
+      ffz: ffzEmoteData,
+    };
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
 };
 
-getRequest()
+const f = async () => {
+  const emoteData = await getRequest();
+  console.log(emoteData);
+};
 
-alert("Hello from BYTV")
+f();
+
+alert("Hello from BYTV");
