6f68b12691a20b08db800f26d51de11a54fda3a7
Alexander <bombblob@gmail.com>
2021-04-03 01:57:36 -0700

    separated function

--- src/background.js
+++ src/background.js
@@ -2,16 +2,20 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+async function sendMessage(msg) {
+  try {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      if (tabs === undefined || tabs.length < 1) return;
+      const activeTab = tabs[0];
+      chrome.tabs.sendMessage(activeTab.id, msg);
+    });
+  } catch (err) {}
+}
+
 // update chat every so many seconds
 async function sendPeriodicMessage(msg, period) {
   for (;;) {
-    try {
-      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-        if (tabs === undefined || tabs.length < 1) return
-        const activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, { message: msg });
-      });
-    } catch(err){}
+    sendMessage({ message: msg });
     await sleep(period); // TODO: not this. Could be better
   }
 }
