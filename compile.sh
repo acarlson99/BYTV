8feab2391d58543d20012777470f948fcbe9ee4b
Alexander <bombblob@gmail.com>
2021-04-06 23:36:55 -0700

    move devtools stuff to src/devtools

--- compile.sh
+++ compile.sh
@@ -2,16 +2,14 @@ set -e
 CHROME=build-chrome
 FIREFOX=build-firefox
 mkdir -p $CHROME
-# ln -sf $(readlink -f ./icon.png) $CHROME/icon.png
-# ln -sf $(readlink -f ./manifest.json) $CHROME/manifest.json
+# assets
 cp $(readlink -f ./icon.png) $CHROME/
 cp $(readlink -f ./manifest.json) $CHROME/
+# browserify
 npx browserify src/content.js -o $CHROME/content.js
 npx browserify src/background.js -o $CHROME/background.js
-# npx browserify src/devtools.js -o $CHROME/devtools.js
-cp src/devtools.js src/panel.js $CHROME
-
-cp src/*.html $CHROME
+# devtools
+cp src/devtools/* $CHROME
 
 # modify compiled chrome API calls for firefox
 mkdir -p $FIREFOX
